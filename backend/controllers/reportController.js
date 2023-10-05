a558abd84807ce95cd6dfc3d052df75aa745f5fd
vidal77 <vidal77-lc@hotmail.com>
2023-10-05 18:19:40 -0300

    realizo auth/report/user controller

---
+++ backend/controllers/reportController.js
@@ -0,0 +1,34 @@
+const db = require('../db');
+
+// Crea un reporte
+exports.newReport = (req, res) => {
+  const { username, fqdn, comentario, valoracion } = req.body;
+
+  // Inserta el reporte en la db
+  const report = { username, fqdn, comentario, valoracion };
+
+  db.query('INSERT INTO reports SET ?', report, (err, result) => {
+    if (err) {
+      return res.status(500).json({ error: 'Report creation failed' });
+    }
+
+    return res.status(201).json({ message: 'Report created successfully' });
+  });
+};
+
+// Get all reports
+exports.getAllReports = (req, res) => {
+  const user = req.user;
+
+  if (user.role === 'admin') {
+    db.query('SELECT * FROM reports', (err, rows) => {
+      if (err) {
+        return res.status(500).json({ error: 'Error fetching reports' });
+      }
+
+      return res.status(200).json(rows);
+    });
+  } else {
+    return res.status(403).json({ error: 'Access denied' });
+  }
+};
---
+++ backend/routes/reportRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { validateReport } = require('../schemas/schemas');
+const jwtMiddleware = require('../middlewares/jwtMiddleware');
+const reportController = require('../controllers/reportController')
+
+
+// Crea un reporte
+router.post('/new', jwtMiddleware.verifyToken, validateReport, reportController.newReport);
+
+// Retorna todos los reportes
+router.post('/all', jwtMiddleware.verifyToken, reportController.getAllReports);
+
+
+module.exports = router;
+
